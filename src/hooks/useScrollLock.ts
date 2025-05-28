97acd2b854a6c8e50840599c19e8e840e44cb720
Park Bomin <parkbm@cle.vision>
2025-05-28 14:25:37 +0900

    fix: 모달창 open 시, 스크롤바 여부에 따라 padding 추가 여부 조건 추가

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -7,8 +7,15 @@ export default function useScrollLock(isLock: boolean) {
     const originalStyle = window.getComputedStyle(document.body);
     const originalOverflow = originalStyle.overflow;
 
+    const hasScrollbar = () => {
+      // 스크롤바 유무 확인
+      return document.body.scrollHeight > window.innerHeight;
+    };
+
     document.body.style.overflow = 'hidden';
-    document.body.style.marginRight = '4px'; // global.css: scrollbar width = 4px
+    if (hasScrollbar()) {
+      document.body.style.marginRight = '4px'; // global.css: scrollbar width = 4px
+    }
 
     return () => {
       document.body.style.overflow = originalOverflow;
